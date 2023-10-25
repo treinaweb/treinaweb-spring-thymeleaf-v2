4b2b9d31fec02b47688c1a24621d1a5a1ae13125
Cleyson <cleysonph@gmail.com>
2023-10-24 22:17:22 -0300

    05:07 - Formatação de dados com Utility Objects

--- src/main/java/br/com/treinaweb/twprojects/web/home/controllers/HomeController.java
+++ src/main/java/br/com/treinaweb/twprojects/web/home/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package br.com.treinaweb.twprojects.web.home.controllers;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -11,6 +17,18 @@ public class HomeController {
     @GetMapping
     public String index() {
         return "home/index";
+    }
+
+    @GetMapping("/format-data")
+    public ModelAndView formatData() {
+        var model = Map.of(
+            "locale", Locale.of("en-us"),
+            "date", LocalDate.of(2024, 10, 10),
+            "datetime", LocalDateTime.of(2024, 10, 10, 10, 10, 10),
+            "currency", 1024.51,
+            "percent", 51.0 / 100
+        );
+        return new ModelAndView("format-data", model);
     }
     
 }
